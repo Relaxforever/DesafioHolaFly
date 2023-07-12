e2218556d1224535dcb764bb262e42d89e81ecfe
Fidel Caicedo <ffcaicedoc@gmail.com>
2023-07-12 14:34:10 -0500

    feat (env y Lint): Agregado env y Linter archivo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-
-
 const bodyParser = require('body-parser');
-
 const createServer = require('./server');
-
 const app = require('./app');
 
 async function start() {
 	const server = await createServer(app);
 
-  server.get('/', (req, res) => {
+	server.use(bodyParser.json());
+	server.get('/', (req, res) => {
     res.send('Welcome to Holafly\'s Technical test!');
   });
 
 	// Start the GraphQL server
-  const port = process.env.PORT || 4567;
+  const port = process.env.PORT || 3000;
+
+
+  console.log('port', port);
+  console.log('process.env.PORT', process.env.PORT);
+  console.log('process.env', process.env.URL_API);
+
 	server.listen(port , () => {
 		// eslint-disable-next-line no-console
 		console.log(`Server is running on port: ${port}`);
